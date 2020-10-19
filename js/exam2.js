ff234ae44d7fb6849546d4be35f81131f63ee382
Tanner Williams <tannerwill756@gmail.com>
2020-10-19 19:02:09 -0400

    exam2 finished

--- js/exam2.js
+++ js/exam2.js
@@ -1,7 +1,20 @@
 var app2 = angular.module('app2', []);
  
 
-app1.controller('ctrl1', function($scope) {
- 
+app2.controller('ctrl1', function($scope) {
+    $scope.randomNum1 = Math.floor((Math.random() * 10) + 1);
+    $scope.randomNum2 = Math.floor((Math.random() * 10) + 1);
   
+});
+
+app2.controller('badCtrl', function($scope) {
+    var badFeelings = ["disregarded", "unimportant", "rejected", "powerless"];
+
+    $scope.bad = badFeelings [Math.floor((Math.random() * 4))];
+});
+
+app2.controller('goodCtrl', function($scope) {
+    var goodFeelings = ["pleasure", "awesome", "lovable", "inner-peace"];
+
+    $scope.good = goodFeelings [Math.floor((Math.random() * 4))];
 });
